cf6574d07a77125e7318924fdc2f9e4e8af8d9e9
Chris McFarlen <cmcfarlen@apple.com>
2024-12-03 12:49:27 -0600

    day3

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "advent2024",
+    platforms: [
+      .macOS(.v14)
+    ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
         .package(url: "https://github.com/apple/swift-algorithms", from: "1.2.0"),
--- Sources/advent2024.swift
+++ Sources/advent2024.swift
@@ -22,22 +22,26 @@ extension Key: ExpressibleByArrayLiteral {
 }
 
 extension Sequence {
+  @inlinable
   func groupBy<T: Hashable>(_ f: (Element) throws -> T) rethrows -> [T: [Element]] { 
     try self.reduce(into: [T:[Element]]()) { r, e in
       r[try f(e), default: []].append(e)
     }
   }
 
+  @inlinable
   func groupByIndexed<T: Hashable>(_ f: (Int, Element) throws -> T) rethrows -> [T: [Element]] { 
     try self.enumerated().reduce(into: [T:[Element]]()) { r, e in
       r[try f(e.offset, e.element), default: []].append(e.element)
     }
   }
 
+  @inlinable
   func sum() -> Element where Element == Int {
     self.reduce(0) { $0 + $1 }
   }
 
+  @inlinable
   func show() -> Self {
     for (line, x) in self.enumerated() {
       print("\(line): \(x)")
@@ -45,6 +49,7 @@ extension Sequence {
     return self
   }
 
+  @inlinable
   func show(_ input: [String]) -> Self {
     for (line, x) in self.enumerated() {
       print("\(line): \(input[line]) -> \(x)")
@@ -52,12 +57,14 @@ extension Sequence {
     return self
   }
 
+  @inlinable
   func frequencies() -> [Element: Int] where Element: Hashable {
     self.reduce(into: [Element:Int]()) { r, e in
       r[e, default: 0] += 1
     }
   }
 
+  @inlinable
   func dropNth(_ i: Int) -> [Element] {
     self
     .enumerated()
@@ -68,12 +75,36 @@ extension Sequence {
 
 }
 
+@inlinable
+func complement<each T>(_ f: @escaping (repeat each T)->Bool) -> (repeat each T) -> Bool {
+  { (args: repeat each T) in
+    !f(repeat each args)
+  }
+}
+
+extension Collection {
+  @inlinable
+  func remove(_ f: @escaping (Element) -> Bool) -> [Element] {
+    filter(complement(f))
+  }
+}
+
 extension String {
   var lines: [String] {
     self.split(separator: "\n").map { String($0) }
   }
   var whitespace: [String] {
     self.split(separator: " ").map { String($0) }
+  }
+
+  var int: Int {
+    Int(self)!
+  }
+}
+
+extension Substring {
+  var int: Int {
+    Int(self)!
   }
 }
 
@@ -177,6 +208,34 @@ enum Day2 {
   }
 }
 
+enum Day3 {
+  static func part1(input: String) -> Int {
+    let regex = /mul\((\d+),(\d+)\)/
+
+    return input
+      .matches(of: regex)
+      .map { m in
+        m.1.int * m.2.int
+      }
+      .sum()
+  }
+
+  static func part2(input: String) -> Int {
+    return input
+      .split(separator: "do")
+      .reductions((true, "")) { last, next in
+        (next.starts(with: "n't()") ? false : next.starts(with: "()") ? true : last.0,
+         next)
+      }
+      .compactMap { x in
+        x.0 ? x.1 : nil
+      }
+      .map(String.init)
+      .map(part1)
+      .sum()
+  }
+}
+
 func slurpInput(day: Int) throws -> String {
     let cwd = FileManager.default.currentDirectoryPath
     let path = "\(cwd)/input/day\(day).txt"
@@ -199,7 +258,9 @@ struct advent2024: ParsableCommand {
       [1, 1]: Day1.part1,
       [1, 2]: Day1.part2,
       [2, 1]: Day2.part1,
-      [2, 2]: Day2.part2
+      [2, 2]: Day2.part2,
+      [3, 1]: Day3.part1,
+      [3, 2]: Day3.part2
     ]
 
     let key: Key = [day, part]
