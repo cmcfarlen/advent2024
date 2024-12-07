a18469a913ad77acb20e32037a45333022c1b92d
Chris McFarlen <cmcfarlen@apple.com>
2024-12-07 17:44:53 -0600

    day7 but back to front

--- Sources/advent2024.swift
+++ Sources/advent2024.swift
@@ -94,6 +94,30 @@ extension Collection {
   }
 }
 
+extension Array {
+  @inlinable
+  func firstRest() -> (Element, SubSequence) {
+    return (self.first!, self.dropFirst())
+  }
+
+  @inlinable
+  func restLast() -> (SubSequence, Element) {
+    return (self.dropLast(), self.last!)
+  }
+}
+
+extension ArraySlice {
+  @inlinable
+  func firstRest() -> (Element, SubSequence) {
+    return (self.first!, self.dropFirst())
+  }
+
+  @inlinable
+  func restLast() -> (SubSequence, Element) {
+    return (self.dropLast(), self.last!)
+  }
+}
+
 extension String {
   public var lines: [String] {
     self.split(separator: "\n").map { String($0) }
@@ -669,14 +693,15 @@ enum Day6 {
 }
 
 enum Day7 {
-  static func combine(_ a: Int, _ b: Int) -> Int {
-    func mag(_ a: Int) -> Int {
-      var m = 10
+  static func mag(_ a: Int) -> Int {
+    var m = 10
       while a / m > 0 {
         m *= 10
       }
-      return m
-    }
+    return m
+  }
+
+  static func combine(_ a: Int, _ b: Int) -> Int {
 
     return a * mag(b) + b
   }
@@ -698,12 +723,37 @@ enum Day7 {
     return rtest(f, rest)
   }
 
+  static func possiblyBackFront(_ answer: Int, _ numbers: [Int]) -> Int? {
+    func rtest(_ a: Int, _ numbers: ArraySlice<Int>) -> Bool {
+      guard numbers.count > 1 else {
+        return a == numbers.last!
+      }
+
+      let (rest, f) = numbers.restLast()
+
+      let (quot, rem) = a.quotientAndRemainder(dividingBy: f)
+      if rem == 0 && rtest(quot, rest) {
+        return true
+      }
+
+      let m = mag(f)
+      let (q, r) = a.quotientAndRemainder(dividingBy: m)
+      if r == f && rtest(q, rest) {
+        return true
+      }
+
+      return rtest(a - f, rest)
+    }
+
+    return rtest(answer, numbers[...]) ? answer : nil
+  }
+
   static func part1(input: String) -> Int {
     return input.lines.map {
       $0.split(separator: ": ")
     }
     .compactMap { v in
-      possiblyTrue(Int(v[0])!, String(v[1]).splitInts(separator: " "))
+      possiblyBackFront(Int(v[0])!, String(v[1]).splitInts(separator: " "))
     }
     .sum()
   }
