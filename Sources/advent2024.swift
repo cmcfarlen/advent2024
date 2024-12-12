24b71fbc7ceab1e5d6efb0f545534658b55327aa
Chris McFarlen <cmcfarlen@apple.com>
2024-12-12 05:55:26 -0600

    day 12 part 1

--- Sources/advent2024.swift
+++ Sources/advent2024.swift
@@ -249,6 +249,13 @@ extension Coordinate {
              [0, 1]].map { self + $0 }
   }
 
+  @inlinable
+  public func adjacent(to a: Coordinate) -> Bool {
+    let dx = diff(a.x, x)
+    let dy = diff(a.y, y)
+    return (dx == 0 && dy == 1) || (dy == 0 && dx == 1)
+  }
+
   public static let origin: Coordinate = .init(x: 0, y: 0)
 }
 
@@ -353,6 +360,12 @@ extension Collection where Element: Collection<Character> {
     } 
   }
 
+  func cleared(with c: Character = ".") -> [[Character]] {
+    let row: [Character] = .init(repeating: c, count: width)
+    let g: [[Character]] = .init(repeating: row, count: height)
+    return g
+  }
+
   var display: String {
     String(map(String.init).joined(by: "\n"))
   }
@@ -388,7 +401,7 @@ extension Substring {
   }
 }
 
-func diff(_ a: Int, _ b: Int) -> Int {
+public func diff(_ a: Int, _ b: Int) -> Int {
   abs(a - b)
 }
 
@@ -1199,6 +1212,74 @@ enum Day11 {
   }
 }
 
+enum Day12 {
+  struct Region {
+    let plant: Character
+    var coords: Set<Coordinate>
+
+    var area: Int {
+      coords.count
+    }
+    var perimeter: Int {
+      func adjCnt(_ c: Coordinate) -> Int {
+        coords.remove { $0 == c }.filter { $0.adjacent(to: c) }.count
+      }
+      return coords.map(adjCnt).map { 4 - $0 }.sum()
+    }
+    var cost: Int {
+      area * perimeter
+    }
+  }
+
+  static func dumpRegion(into grid: inout [[Character]], _ r: Region) -> [[Character]] {
+    for c in r.coords {
+      grid[c] = r.plant
+    }
+
+    return grid
+  }
+
+  static func createRegions(for grid: [[Character]]) -> [Region] {
+    return grid.coords.reduce(into: []) { regions, coord in
+      let plant = grid[coord]!
+      let idxs = regions.indices.filter { idx in
+        regions[idx].plant == plant && regions[idx].coords.contains { x in x.adjacent(to: coord) }
+      }
+      if idxs.count == 0 {
+        regions.append(Region(plant: plant, coords: [coord]))
+      } else if idxs.count == 1 {
+        regions[idxs[0]].coords.insert(coord)
+      } else {
+        regions[idxs[0]] = idxs.dropFirst().reduce(into: regions[idxs[0]]) { r, idx in
+          r.coords = r.coords.union(regions[idx].coords)
+        }
+        regions[idxs[0]].coords.insert(coord)
+        for idx in idxs.dropFirst() {
+          regions.remove(at: idx)
+        }
+      }
+    }
+  }
+
+  static func part1(input: String) -> Int {
+    let grid = input.grid
+    let regions = createRegions(for: grid)
+
+/*
+    var t = grid.cleared()
+    for r in regions {
+      print("plant \(r.plant) area \(r.area) * \(r.perimeter) = \(r.cost) ")
+      _ = dumpRegion(into: &t, r)
+    }
+
+    print(t.display)
+    assert(t == grid)
+    */
+
+    return regions.map(\.cost).sum()
+  }
+}
+
 func slurpInput(day: Int) throws -> String {
     let cwd = FileManager.default.currentDirectoryPath
     let path = "\(cwd)/input/day\(day).txt"
@@ -1258,6 +1339,7 @@ struct advent2024: AsyncParsableCommand {
       [10, 2]: Day10.part2,
       [11, 1]: Day11.part1,
       [11, 2]: Day11.part2smart,
+      [12, 1]: Day12.part1,
     ]
 
     let registryAsync: [Key: DayFunctionAsync] = [
