4c78571e970da5cadf8875732dc4270eba6ca3a5
Chris McFarlen <cmcfarlen@apple.com>
2024-12-24 07:30:46 -0600

    day21 wip

--- Sources/day21.swift
+++ Sources/day21.swift
@@ -37,6 +37,74 @@ enum Day21 {
     }
   }
 
+  enum Orientation {
+    case horizontal
+    case vertical
+  }
+
+  protocol PositionStrategy {
+    func move(from src: Coordinate, to dest: Coordinate, gap: Coordinate) -> [Dir]
+  }
+
+  struct OGPositionStrategy: PositionStrategy {
+    func move(from src: Coordinate, to dest: Coordinate, gap: Coordinate) -> [Dir] {
+      var result: [Dir] = []
+      var p = src
+
+      // hor then vert or vert then hor
+      for var delta in safeMoves(from: src, to: dest, gap: gap) {
+        while let d = Dir.dirFor(delta) {
+          p = p + d.offset
+          assert(p != gap, "Positioned over gap!")
+          result.append(d)
+
+          delta = delta + d.offset
+        }
+      }
+      return result
+    }
+  }
+
+  struct MajorPositionStrategy: PositionStrategy {
+    let orientation: Orientation
+
+    func move(from src: Coordinate, to dest: Coordinate, gap: Coordinate) -> [Dir] {
+      var result: [Dir] = []
+      var next: Coordinate = src
+
+      let moves = movements(from: src, to: dest, gap: gap)
+      for var delta in moves {
+        while let d = Dir.dirFor(delta) {
+          next = next + d.offset
+          assert(next != gap, "Positioned over gap! \(src)->\(dest) \(gap) \(next) \(delta) \(moves)")
+          result.append(d)
+
+          delta = delta + d.offset
+        }
+      }
+
+      return result
+    }
+
+    func movements(from src: Coordinate, to dest: Coordinate, gap: Coordinate) -> [Coordinate] {
+      let delta = src - dest
+      let (hor, vert): (Coordinate, Coordinate) = ([delta.x, 0], [0, delta.y])
+      switch orientation {
+        case .horizontal:
+          if dest.x == gap.x && src.y == gap.y {
+            return [[hor.x-1, 0], vert, [1, 0]]
+          }
+          return [hor, vert]
+        case .vertical:
+          if dest.y == gap.y && src.x == gap.x {
+            let gdir = Dir.dirFor(gap - dest)!.offset
+            return [vert-gdir, hor, vert+gdir]
+          }
+          return [vert, hor]
+      }
+    }
+  }
+
   struct Robot {
     let keys: [[Character]]
     let gap: Coordinate
@@ -52,40 +120,17 @@ enum Day21 {
       keys.coords.first { keys[$0] == key }!
     }
 
-    // mind the gap
-    // if the destination's y is the gaps y and the source x is the gap's x
-    // go the other direction first (up/down or left/right)
-    func safeMoves(to dest: Coordinate) -> [Coordinate] {
-      let delta = p - dest
-      let (hor, vert): (Coordinate, Coordinate) = ([delta.x, 0], [0, delta.y])
-      if dest.x == gap.x && p.y == gap.y {
-        return [vert, hor]
-      }
-      return [hor, vert]
-    }
-
-    mutating func move(to key: Character) -> [Dir] {
+    mutating func move(to key: Character, strategy: PositionStrategy) -> [Dir] {
       let dest = position(key: key)
-      var result: [Dir] = []
-
-      // hor then vert or vert then hor
-      for var delta in safeMoves(to: dest) {
-        while let d = Dir.dirFor(delta) {
-          p = p + d.offset
-          assert(p != gap, "Positioned over gap!")
-          result.append(d)
-
-          delta = delta + d.offset
-        }
-      }
-
+      let result = strategy.move(from: p, to: dest, gap: gap)
+      p = dest
       return result
     }
 
-    mutating func output(sequence: String) -> [[Dir]] {
+    mutating func output(sequence: String, strategy strat: PositionStrategy) -> [[Dir]] {
       var result: [[Dir]] = []
       for k in sequence {
-        result.append(move(to: k))
+        result.append(move(to: k, strategy: strat))
       }
       return result
     }
@@ -116,10 +161,10 @@ enum Day21 {
       Int(code.drop { $0 == "0" }.dropLast())!
     }
 
-    func solve(for code: String) -> Int {
+    func solve(for code: String, strategy: PositionStrategy) -> Int {
       var sequence = code
       for idx in bots.indices {
-        sequence = String(bots[idx].output(sequence: sequence)
+        sequence = String(bots[idx].output(sequence: sequence, strategy: strategy)
           .map {
             $0.map(\.symbol).joined()
           }
@@ -131,12 +176,12 @@ enum Day21 {
       return sequence.count
     }
 
-    func debug(for code: String) -> Int {
+    func debug(for code: String, strategy: PositionStrategy) -> Int {
       var botSeq: [[(String, Character)]] = Array(repeatElement([], count: bots.count))
       for l in code {
         var sequence = String(l)
         for idx in bots.indices {
-          sequence = String(bots[idx].output(sequence: sequence)
+          sequence = String(bots[idx].output(sequence: sequence, strategy: strategy)
             .map {
               $0.map(\.symbol).joined()
             }
@@ -157,11 +202,29 @@ enum Day21 {
     }
 
     func complexity(for code: String) -> Int {
-      numericPart(of: code) * solve(for: code)
+      numericPart(of: code) * solve(for: code, strategy: OGPositionStrategy())
     }
 
-    debug(for: "379A")
+    debug(for: "379A", strategy: OGPositionStrategy())
+    print("Vertical major")
+    debug(for: "379A", strategy: MajorPositionStrategy(orientation: .vertical))
+    print("Horizontal major")
+    debug(for: "379A", strategy: MajorPositionStrategy(orientation: .horizontal))
 
     return codes.map(complexity).sum()
+  }
+}
+
+extension Day21.PositionStrategy {
+  // mind the gap
+  // if the destination's y is the gaps y and the source x is the gap's x
+  // go the other direction first (up/down or left/right)
+  func safeMoves(from p: Coordinate, to dest: Coordinate, gap: Coordinate) -> [Coordinate] {
+    let delta = p - dest
+    let (hor, vert): (Coordinate, Coordinate) = ([delta.x, 0], [0, delta.y])
+    if dest.x == gap.x && p.y == gap.y {
+      return [vert, hor]
+    }
+    return [hor, vert]
   }
 }
