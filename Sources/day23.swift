0884d36cfcafd74ba348d4f0f93662e93d58eb12
Chris McFarlen <cmcfarlen@apple.com>
2024-12-25 06:34:18 -0600

    day23 part2

--- Sources/day23.swift
+++ Sources/day23.swift
@@ -26,4 +26,39 @@ enum Day23 {
 
     return tlans.count
   }
+
+  static func part2(input: String) -> String {
+    let pairs = input.lines
+      .map {
+        $0.split(separator: "-").map(String.init)
+      }
+    let connectedTo = pairs.reduce(into: [String:Set<String>]()) { s, p in
+      s[p[0], default: []].insert(p[1])
+      s[p[1], default: []].insert(p[0])
+    }
+    func areConnected(_ a: String, _ b: String) -> Bool {
+      connectedTo[a]?.contains(b) ?? false
+    }
+
+    // https://en.wikipedia.org/wiki/Bron–Kerbosch_algorithm
+    func maxClique(r: Set<String>, p: Set<String>, x: Set<String>) -> [Set<String>] {
+      if p.isEmpty && x.isEmpty {
+        return [r]
+      }
+      var result: [Set<String>] = []
+      var pp = p
+      var xx = x
+      for v in p {
+        let n = connectedTo[v] ?? []
+        result.append(contentsOf: maxClique(r: r.union([v]), p: pp.intersection(n), x: xx.intersection(n)))
+        pp.remove(v)
+        xx.insert(v)
+      }
+      return result
+    }
+
+    let max = maxClique(r: [], p: Set<String>(connectedTo.keys), x: []).sorted { $0.count > $1.count }.first!
+
+    return max.sorted().joined(separator: ",")
+  }
 }
